837ac2478dba76d9e94ccb3cc9a6906888f466fe
Hamdy <tantaacademyt196@gmail.com>
2025-03-05 03:57:19 +0000

    Add: Filter Component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Card from "../utils/Card";
 import Search from "../utils/Search";
 import weatherData from "../../fakeData/weather.json"
+import Filter from "../utils/Filters";
 
 function Home() {
 
@@ -8,8 +9,9 @@ function Home() {
       <section className="py-[30px]">
         <div className="container flex flex-col items-center gap-y-[45px]">
             <h1 className="max-w-[778px] w-full text-[clamp(25px,_5vw,_40px)] text-center leading-[1.3] font-medium text-[--white-color]">Seeing the weather of the whole world with <span className="text-[#78CFFF]">Dark Weather!</span></h1>
-            <div className="max-w-[630px] w-full">
+            <div className="flex items-center justify-around flex-wrap gap-x-[30px] gap-y-[25px] w-full">
                 <Search />
+                <Filter />
             </div>
             <div className="grid grid-cols-[repeat(auto-fit,min(350px,_100%))] justify-center items-center gap-[30px] w-full">
               {
--- src/components/utils/Filters.jsx
+++ src/components/utils/Filters.jsx
@@ -1,15 +1,26 @@
 function Filter() {
 
     return(
-        // <div>
-        //     <span>Filters</span>
-        //     <select name="" id="" className="">
-        //         <option value="" disabled></option>
-        //         <option value="coldest">coldest </option>
-        //         <option value="hot">Hot</option>
-        //     </select>
-        // </div>
-        <></>
+        <div className="flex items-center justify-center flex-wrap gap-5 py-4 px-8 rounded-lg bg-[#24343D]">
+            <h2 className="text-xl font-semibold text-white">Filters</h2>
+            <div className=" flex items-center justify-center flex-wrap gap-9">
+            <select className="w-[130px] py-2 px-1 rounded-[9px] outline-none border border-[#FFFFFF78]  bg-[#24343D]  text-white" id="temperatureFilter" name="temperature">
+                <option value="coldest">Coldest</option>
+                <option value="hottest">Hottest</option>
+            </select>
+            <select className="w-[130px] py-2 px-1 rounded-[9px] outline-none border border-[#FFFFFF78]  bg-[#24343D]  text-white" id="rangeFilter" name="temperatureRange">
+                <option value="0-10">0°C - 10°C</option>
+                <option value="10-20">10°C - 20°C</option>
+                <option value="20-30">20°C - 30°C</option>
+                <option value="30+">Above 30°C</option>
+            </select>
+            <select className="w-[130px] py-2 px-1 rounded-[9px] outline-none border border-[#FFFFFF78]  bg-[#24343D]  text-white" id="weatherFilter" name="weatherType">
+                <option value="rainy">Rainy</option>
+                <option value="sunny">Sunny</option>
+                <option value="cloudy">Cloudy</option>
+            </select>
+            </div>
+        </div>
     )
 }
 export default Filter;
